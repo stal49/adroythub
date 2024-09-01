81c9ef72592260514fc2c58f69ee0909455ab66f
stal49 <smartcode49@gmail.com>
2024-09-01 23:12:23 +0530

    update link3

--- server/build/app.js
+++ server/build/app.js
@@ -24,7 +24,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000"],
+    origin: ["https://adroythub.netlify.app"],
     credentials: true,
 }));
 // api requests limit
@@ -35,7 +35,7 @@ const limiter = (0, express_rate_limit_1.rateLimit)({
     legacyHeaders: false,
 });
 // routes
-exports.app.use("/api/v1", user_route_1.default, order_route_1.default, course_route_1.default, notification_route_1.default, analytics_route_1.default, layout_route_1.default, internship_route_1.default);
+exports.app.use("/api/v1/", user_route_1.default, order_route_1.default, course_route_1.default, notification_route_1.default, analytics_route_1.default, layout_route_1.default, internship_route_1.default);
 // testing api
 exports.app.get("/test", (req, res, next) => {
     res.status(200).json({
