5b2553547bcfe66823b3880151872646410362cf
stal49 <smartcode49@gmail.com>
2024-09-02 00:01:27 +0530

    update coursespage

--- client/app/courses/page.tsx
+++ client/app/courses/page.tsx
@@ -1,8 +1,9 @@
 "use client";
+
 import { useGetUsersAllCoursesQuery } from "@/redux/features/courses/coursesApi";
 import { useGetHeroDataQuery } from "@/redux/features/layout/layoutApi";
 import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import Loader from "../components/Loader/Loader";
 import Header from "../components/Header";
 import Heading from "../utils/Heading";
@@ -19,20 +20,20 @@ const Page = (props: Props) => {
   const { data: categoriesData } = useGetHeroDataQuery("Categories", {});
   const [route, setRoute] = useState("Login");
   const [open, setOpen] = useState(false);
-  const [courses, setcourses] = useState([]);
+  const [courses, setCourses] = useState([]);
   const [category, setCategory] = useState("All");
 
   useEffect(() => {
     if (category === "All") {
-      setcourses(data?.courses);
+      setCourses(data?.courses);
     }
     if (category !== "All") {
-      setcourses(
+      setCourses(
         data?.courses.filter((item: any) => item.categories === category)
       );
     }
     if (search) {
-      setcourses(
+      setCourses(
         data?.courses.filter((item: any) =>
           item.name.toLowerCase().includes(search.toLowerCase())
         )
@@ -64,46 +65,50 @@ const Page = (props: Props) => {
               }
             />
             <br />
-            <div className="w-full flex items-center flex-wrap">
-              <div
-                className={`h-[35px] ${
-                  category === "All" ? "bg-[crimson]" : "bg-[#5050cb]"
-                } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
-                onClick={() => setCategory("All")}
-              >
-                All
-              </div>
-              {categories &&
-                categories.map((item: any, index: number) => (
-                  <div key={index}>
-                    <div
-                      className={`h-[35px] ${
-                        category === item.title
-                          ? "bg-[crimson]"
-                          : "bg-[#5050cb]"
-                      } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
-                      onClick={() => setCategory(item.title)}
-                    >
-                      {item.title}
+            <Suspense fallback={<Loader />}>
+              <div className="w-full flex items-center flex-wrap">
+                <div
+                  className={`h-[35px] ${
+                    category === "All" ? "bg-[crimson]" : "bg-[#5050cb]"
+                  } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
+                  onClick={() => setCategory("All")}
+                >
+                  All
+                </div>
+                {categories &&
+                  categories.map((item: any, index: number) => (
+                    <div key={index}>
+                      <div
+                        className={`h-[35px] ${
+                          category === item.title
+                            ? "bg-[crimson]"
+                            : "bg-[#5050cb]"
+                        } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
+                        onClick={() => setCategory(item.title)}
+                      >
+                        {item.title}
+                      </div>
                     </div>
-                  </div>
-                ))}
-            </div>
-            {
-                courses && courses.length === 0 && (
-                    <p className={`${styles.label} justify-center min-h-[50vh] flex items-center`}>
-                    {search ? "No courses found!" : "No courses found in this category. Please try another one!"}
-                  </p>
-                )
-            }
-            <br />
-            <br />
-            <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
-              {courses &&
-                courses.map((item: any, index: number) => (
-                  <CourseCard item={item} key={index} />
-                ))}
-            </div>
+                  ))}
+              </div>
+              {courses && courses.length === 0 && (
+                <p
+                  className={`${styles.label} justify-center min-h-[50vh] flex items-center`}
+                >
+                  {search
+                    ? "No courses found!"
+                    : "No courses found in this category. Please try another one!"}
+                </p>
+              )}
+              <br />
+              <br />
+              <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
+                {courses &&
+                  courses.map((item: any, index: number) => (
+                    <CourseCard item={item} key={index} />
+                  ))}
+              </div>
+            </Suspense>
           </div>
           <Footer />
         </>
