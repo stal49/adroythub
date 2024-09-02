88cb0da43e9d3cb9f69c8d2539538bc109c7d382
stal49 <smartcode49@gmail.com>
2024-09-02 10:47:19 +0530

    added localhost2

--- server/build/app.js
+++ server/build/app.js
@@ -24,7 +24,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ["https://adroythub.vercel.app"],
+    origin: ["https://adroythub.vercel.app", "https://localhost:3000"],
     credentials: true,
 }));
 // api requests limit
