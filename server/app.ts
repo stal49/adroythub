c51c5ceb258d0a3cbb1ab3fd3f9347f89d5a7435
stal49 <smartcode49@gmail.com>
2024-09-02 14:07:55 +0530

    new cors update

--- server/app.ts
+++ server/app.ts
@@ -22,10 +22,13 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://adroythub.vercel.app", "https://localhost:3000"],
+    origin: ["https://adroythub.vercel.app", "http://localhost:3000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 // api requests limit
 const limiter = rateLimit({
