54bb7569e079dbe86d48992ecf354329abb25329
stal49 <smartcode49@gmail.com>
2024-09-02 15:51:03 +0530

    new css  update2

--- client/app/courses/CoursesComponent.tsx
+++ client/app/courses/CoursesComponent.tsx
@@ -42,7 +42,7 @@ const CoursesComponent = () => {
     <>
       <div className="w-full flex items-center flex-wrap">
         <div
-          className={`h-[35px] ${
+          className={`h-[35px] text-white dark:text-white ${
             category === "All" ? "bg-[crimson]" : "bg-[#5050cb]"
           } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
           onClick={() => setCategory("All")}
@@ -53,7 +53,7 @@ const CoursesComponent = () => {
           categories.map((item: any, index: number) => (
             <div key={index}>
               <div
-                className={`h-[35px] ${
+                className={`h-[35px] text-white dark:text-white ${
                   category === item.title ? "bg-[crimson]" : "bg-[#5050cb]"
                 } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
                 onClick={() => setCategory(item.title)}
