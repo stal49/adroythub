ce9ea7c17cd6c7771b2550b8a4aa8b875d752c63
stal49 <smartcode49@gmail.com>
2024-09-03 15:06:47 +0530

    new review photo

--- client/app/components/Route/Reviews.tsx
+++ client/app/components/Route/Reviews.tsx
@@ -8,41 +8,41 @@ type Props = {};
 export const reviews = [
   {
     name: "Ravi Sharma",
-    avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+    avatar: require('@/public/assests/user4.jpg'),
     profession: "Student | Pune university",
     comment:
     "I had the pleasure of exploring Adroythub, a website that provides an extensive range of courses on various tech-related topics. I was thoroughly impressed with my experience, as the website offers a comprehensive selection of courses that cater to different skill levels and interests. If you're looking to enhance your knowledge and skills in the tech industry, I highly recommend checking out Becodemy!",
 },
   {
     name: "Priya Desai",
-    avatar: "https://randomuser.me/api/portraits/women/1.jpg",
+    avatar: require('@/public/assests/user2.jpg'),
     profession: "Full stack developer | Quarter ltd.",
     comment:
     "Thanks for your amazing programming tutorial channel! Your teaching style is outstanding, and the quality of your tutorials is top-notch. Your ability to break down complex topics into manageable parts, and cover diverse programming languages and topics is truly impressive. The practical applications and real-world examples you incorporate reinforce the theoretical knowledge and provide valuable insights. Your engagement with the audience fosters a supportive learning environment. Thank you for your dedication, expertise, and passion for teaching programming, and keep up the fantastic work!",
 },
   {
     name: "Vikram Rao",
-    avatar: "https://randomuser.me/api/portraits/men/2.jpg",
+    avatar: require('@/public/assests/user6.jpg'),
     profession: "computer systems engineering student | India",
     comment:
     "Thanks for your amazing programming tutorial channel! Your teaching style is outstanding, and the quality of your tutorials is top-notch. Your ability to break down complex topics into manageable parts, and cover diverse programming languages and topics is truly impressive. The practical applications and real-world examples you incorporate reinforce the theoretical knowledge and provide valuable insights. Your engagement with the audience fosters a supportive learning environment. Thank you for your dedication, expertise, and passion for teaching programming, and keep up the fantastic work!"},
   {
     name: "Aditi Singh",
-    avatar: "https://randomuser.me/api/portraits/women/2.jpg",
+    avatar: require('@/public/assests/user1.jpg'),
     profession: "Junior Web Developer | Pune",
     comment:
     "I had the pleasure of exploring Adroythub, a website that provides an extensive range of courses on various tech-related topics. I was thoroughly impressed with my experience",
 },
   {
     name: "Sneha Patil",
-    avatar: "https://randomuser.me/api/portraits/women/3.jpg",
+    avatar: require('@/public/assests/user3.jpg'),
     profession: "Full stack web developer | Mumbai",
     comment:
     "Your content is very special. The thing I liked the most is that the videos are so long, which means they cover everything in details. for that any person had beginner-level can complete an integrated project when he watches the videos. Thank you very much. Im very excited for the next videos Keep doing this amazing work",
 },
   {
     name: "Neha Verma",
-    avatar: "https://randomuser.me/api/portraits/women/4.jpg",
+    avatar: require('@/public/assests/user7.jpg'),
     profession: "Full stack web developer | Jaipur",
     comment:
     "Join Adroythub! Adroythub focuses on practical applications rather than just teaching the theory behind programming languages or frameworks. I took a lesson on creating a web marketplace using React JS, and it was very helpful in teaching me the different stages involved in creating a project from start to finish. Overall, I highly recommend Adroythub to anyone looking to improve their programming skills and build practical projects. Adroythub is a great resource that will help you take your skills to the next level.",
