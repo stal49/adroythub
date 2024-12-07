5686141b53a73499aec5c7e398f56a4ae2591651
karpeomkar608 <karpeomkar608@gmail.com>
2024-12-07 16:32:54 +0530

    drive update1.5

---
+++ client/app/components/Registeration/RegisterationForm.tsx
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+
+interface EnrollmentFormProps {
+  onClose: () => void;
+}
+
+const EnrollmentForm: React.FC<EnrollmentFormProps> = ({ onClose }) => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    mobileNumber: "",
+    College: "",
+    Current_Year: "",
+    Address : "",
+    Course_Interested : "",
+    
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://adroythub.onrender.com/api/v1/registeration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Response error:", errorData);
+        throw new Error(errorData.message || "Failed to submit form");
+      }
+
+      const data = await response.json();
+      toast.success("Enrollment successful!");
+      onClose();
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="relative w-[95%] 800px:w-[40%] bg-gray-100 dark:bg-gray-800 rounded-2xl p-5">
+        <button
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
+          onClick={onClose}
+        >
+          Close
+        </button>
+        <h2 className="text-[24px] p-2 font-semibold">Registeration Form for Adroythub</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              Full Name:
+            </label>
+            <input
+              type="text"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              Email Address:
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              Mobile Number:
+            </label>
+            <input
+              type="text"
+              name="mobileNumber"
+              value={formData.mobileNumber}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white"
+              placeholder="Your Phone Number"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              College:
+            </label>
+            <input
+              type="text"
+              name="courseCompleted"
+              value={formData.College}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white"
+              placeholder="Name of Course completed on Adroythub"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              Current Year:
+            </label>
+            <input
+              type="text"
+              name="highestQualification"
+              value={formData.Current_Year}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white"
+              placeholder="Your highest qualification and field of study"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              Address:
+            </label>
+            <input
+              type="text"
+              name="highestQualification"
+              value={formData.Address}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white"
+              placeholder="Your highest qualification and field of study"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+              Course Interested:
+            </label>
+            <input
+              type="text"
+              name="highestQualification"
+              value={formData.Course_Interested}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white"
+              placeholder="Your highest qualification and field of study"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-purple-500 text-white p-2 rounded-2xl mt-4"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EnrollmentForm;
--- client/app/components/Route/Courses.tsx
+++ client/app/components/Route/Courses.tsx
@@ -1,6 +1,7 @@
 import { useGetUsersAllCoursesQuery } from "@/redux/features/courses/coursesApi";
 import React, { useEffect, useState } from "react";
 import CourseCard from "../Course/CourseCard";
+import RegisterationForm from "../../components/Registeration/RegisterationForm";
 
 type Props = {};
 
@@ -12,6 +13,15 @@ const Courses = (props: Props) => {
     setCourses(data?.courses);
   }, [data]);
 
+  const [isFormVisible, setFormVisible] = useState(false);
+
+    const toggleFormVisibility = () => {
+        setFormVisible((prev) => !prev);
+    };
+    const closeForm = () => {
+      setFormVisible(false);
+  };
+
   return (
     <div>
       <div className={`w-[90%] 800px:w-[80%] m-auto`}>
@@ -21,7 +31,9 @@ const Courses = (props: Props) => {
           Opportunity With Our Courses
         </h1>
         <br />
-        <button className="text-xs sm:text-sm md:text-base bg-purple-500 p-2 sm:p-3 md:p-4 rounded-xl sm:rounded-2xl text-white dark:text-white mt-4"> Register Now!</button>
+        <button className="items-center text-xs sm:text-sm md:text-base bg-purple-500 p-2 sm:p-3 md:p-4 rounded-xl sm:rounded-2xl text-white dark:text-white mt-4"
+         onClick={toggleFormVisibility}> Register Now!</button>
+        {isFormVisible && <RegisterationForm onClose={closeForm} />}
         <br />
         <br />
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
