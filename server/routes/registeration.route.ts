5506c9eb8be90addaea7a9bc60f16ab0315506b4
pratik belote <pratikbelote@MacBookAir.lan>
2025-04-11 22:48:14 +0530

    remove server

--- server/routes/registeration.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createRegisteration } from "../controllers/registeration.controller";
-
-
-const RegisterationRouter = express.Router();
-
-RegisterationRouter.post("/registeration", createRegisteration);
-
-export default RegisterationRouter;
